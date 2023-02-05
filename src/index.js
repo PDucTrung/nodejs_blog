918eb9937519448f0ddb613abdbe3f7efb59d428
trung <trungezeal115@gmail.com>
2023-02-05 18:00:45 +0700

    bai 3

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // http logger
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // template engine setup
 app.engine(
@@ -22,13 +27,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Routes init
+route(app);
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
